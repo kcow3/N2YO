dcbc959e99b938911d7840a92ec71f39e9c29919
Kevin Cowdrey <kevin@gendac.co.za>
2020-02-05 10:02:52 +0200

    Add project files.

---
+++ Kcow3.N2YO.Cmd/Models/TleReslult.cs
@@ -0,0 +1,15 @@
+﻿namespace Kcow3.N2YO.Cmd.Models
+{
+    public class TleResult
+    {
+        public string Tle { get; set; }
+        public Info Info { get; set; }
+    }
+
+    public class Info
+    {
+        public int SatId { get; set; }
+        public string SatName { get; set; }
+        public int TransactionsCount { get; set; }
+    }
+}
---
+++ Kcow3.N2YO.Cmd/Program.cs
@@ -0,0 +1,26 @@
+﻿using Kcow3.N2YO.Cmd.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Kcow3.N2YO.Cmd
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        private static string baseUri = "https://www.n2yo.com/rest/v1/satellite/";
+        private static readonly string apiKey = "Key Here";
+
+        static async Task Main(string[] args)
+        {
+            var req = await client.GetAsync($"https://www.n2yo.com/rest/v1/satellite/tle/25544&apiKey={apiKey}");
+
+            HttpResponseMessage response = await client.GetAsync($"https://www.n2yo.com/rest/v1/satellite/tle/25544&apiKey={apiKey}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var tle = JsonConvert.DeserializeObject<TleResult>(await req.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
